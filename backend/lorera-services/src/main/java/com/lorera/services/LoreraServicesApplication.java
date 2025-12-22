c3c24f81dabe834de347c7d1a6c1040b9f757e9a
Rishi Nivedan <rishinvdn03@gmail.com>
2025-12-23 00:04:49 +0530

    Initial LoreRa monorepo with Spring Boot backend

---
+++ backend/lorera-services/src/main/java/com/lorera/services/LoreraServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lorera.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoreraServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoreraServicesApplication.class, args);
+	}
+
+}
---
+++ backend/lorera-services/src/test/java/com/lorera/services/LoreraServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorera.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoreraServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
